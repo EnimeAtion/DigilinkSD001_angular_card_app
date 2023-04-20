d8ef9e05d709d6a180257abd0291a6119b70db22
EnimeAtion <given.mutsh@icloud.com>
2023-04-20 07:35:29 +0200

    first cards app commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  // object array of details to be included in the card app
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/img/tree.jpeg',
+      username: '@nature',
+      content: 'I saw this neat tree today'
+    }
+    ,
+    { 
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/img/mountain.jpeg', 
+      username: '@mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    }
+    ,
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/img/biking.jpeg',
+      username: '@bikinglover',
+      content: 'I did some biking today'
+      
+    }
+  ];
 }
